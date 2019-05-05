ffef846cb15e1483f5cd83ba885221ad3bab3c3d
bob <channigreenwall@10-16-187-81.dynapool.wireless.nyu.edu>
2019-05-05 17:27:45 -0400

    fixed hw4

---
+++ Final/arne_sprites.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="2019.04.18" name="arne_sprites" tilewidth="16" tileheight="16" tilecount="128" columns="16">
+ <image source="NYUCodebase/arne_sprites.png" width="256" height="128"/>
+</tileset>
